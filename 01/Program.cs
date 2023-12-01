d62c982881528196826b0b39e7286f8d8277d82c
chwun <48294676+chwun@users.noreply.github.com>
2023-12-01 12:41:59 +0100

    day 01 - part 1

---
+++ 01/Program.cs
@@ -0,0 +1,37 @@
+﻿//Dictionary<string, int> lettersToDigits = new()
+//{
+//	["one"] = 1,
+//	["two"] = 2,
+//	["three"] = 3,
+//	["four"] = 4,
+//	["five"] = 5,
+//	["six"] = 6,
+//	["seven"] = 7,
+//	["eight"] = 8,
+//	["nine"] = 9
+//};
+
+string[] lines = File.ReadAllLines("input.txt");
+
+int sum1 = GetSum(lines);
+Console.WriteLine($"part 1: {sum1}");
+
+//int sum2 = GetSum(lines.Select(ConvertLettersToDigits));
+//Console.WriteLine($"part 2: {sum2}");
+
+int GetSum(IEnumerable<string> lines)
+{
+	return lines
+		.Select(l => string.Join("", l.First(char.IsDigit), l.Last(char.IsDigit)))
+		.Sum(int.Parse);
+}
+
+//string ConvertLettersToDigits(string input)
+//{
+//	foreach ((string letters, int digit) in lettersToDigits)
+//	{
+//		input = input.Replace(letters, digit.ToString());
+//	}
+
+//	return input;
+//}
