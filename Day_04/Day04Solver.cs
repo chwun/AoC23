7ecdac35bc013a92d44207730c38f69bd6380ad0
chwun <48294676+chwun@users.noreply.github.com>
2023-12-04 20:41:50 +0100

    day 04 - part 2

--- Day_04/Day04Solver.cs
+++ Day_04/Day04Solver.cs
@@ -1,32 +1,66 @@
-﻿using System.Text.RegularExpressions;
-using Base;
+﻿using Base;
+using System.Text.RegularExpressions;
 
 namespace Day_04;
 
 public class Day04Solver : SolverBase
 {
-	public override int SolvePart1(IEnumerable<string> lines)
-	{
-		int sum = 0;
+    public override int SolvePart1(IEnumerable<string> lines)
+    {
+        int sum = 0;
 
-		foreach (string line in lines)
-		{
-			Match match = Regex.Match(line, @"Card[0-9\s]+:(?<winningNumbers>[0-9\s]+)\|(?<ownNumbers>[0-9\s]+)");
-			if (match.Success)
-			{
-				var winningNumbers = match.Groups["winningNumbers"].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToHashSet();
-				var ownNumbers = match.Groups["ownNumbers"].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToHashSet();
+        foreach (string line in lines)
+        {
+            Match match = Regex.Match(line, @"Card[0-9\s]+:(?<winningNumbers>[0-9\s]+)\|(?<ownNumbers>[0-9\s]+)");
+            if (match.Success)
+            {
+                var winningNumbers = match.Groups["winningNumbers"].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToHashSet();
+                var ownNumbers = match.Groups["ownNumbers"].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToHashSet();
 
-				var ownWinningNumbers = winningNumbers.Intersect(ownNumbers);
-				sum += (int)Math.Pow(2, ownWinningNumbers.Count() - 1);
-			}
-		}
+                int numberOfOwnWinningNumbers = winningNumbers.Intersect(ownNumbers).Count();
+                sum += (int)Math.Pow(2, numberOfOwnWinningNumbers - 1);
+            }
+        }
 
-		return sum;
-	}
+        return sum;
+    }
 
-	public override int SolvePart2(IEnumerable<string> lines)
-	{
-		return 0;
-	}
+    public override int SolvePart2(IEnumerable<string> lines)
+    {
+        int instances = 0;
+
+        Dictionary<int, int> instancesPerCardNumber = new();
+
+        foreach (string line in lines)
+        {
+            Match match = Regex.Match(line, @"Card(?<cardNumber>[0-9\s]+):(?<winningNumbers>[0-9\s]+)\|(?<ownNumbers>[0-9\s]+)");
+            if (match.Success)
+            {
+                int cardNumber = int.Parse(match.Groups["cardNumber"].Value);
+
+                if (!instancesPerCardNumber.ContainsKey(cardNumber))
+                {
+                    instancesPerCardNumber[cardNumber] = 0;
+                }
+
+                var winningNumbers = match.Groups["winningNumbers"].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToHashSet();
+                var ownNumbers = match.Groups["ownNumbers"].Value.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToHashSet();
+
+                int numberOfOwnWinningNumbers = winningNumbers.Intersect(ownNumbers).Count();
+                for (int i = cardNumber + 1; i <= cardNumber + numberOfOwnWinningNumbers; i++)
+                {
+                    if (!instancesPerCardNumber.ContainsKey(i))
+                    {
+                        instancesPerCardNumber[i] = 0;
+                    }
+
+                    instancesPerCardNumber[i] += instancesPerCardNumber[cardNumber] + 1;
+                }
+
+                instances += instancesPerCardNumber.TryGetValue(cardNumber, out int numberOfInstances) ? numberOfInstances + 1 : 1;
+            }
+        }
+
+        return instances;
+    }
 }
